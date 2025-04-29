e9e1c3b50c34a5ead238a15c577b998c2d01d5b8
Mr Lii <leetamparega@gmail.com>
2025-04-29 22:02:26 +0000

    docs: add the methods loginfo, logwarning, logError, and logdebug

--- src/main/java/lii/autotextprocessor4dataflow/util/AppLogger.java
+++ src/main/java/lii/autotextprocessor4dataflow/util/AppLogger.java
@@ -1,4 +1,36 @@
 package lii.autotextprocessor4dataflow.util;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class AppLogger {
+
+    private static final Logger logger = Logger.getLogger(AppLogger.class.getName());
+
+    static {
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.ALL);
+        consoleHandler.setFormatter(new SimpleFormatter());
+        logger.addHandler(consoleHandler);
+        logger.setLevel(Level.ALL);
+        logger.setUseParentHandlers(false);
+    }
+
+    public static void logInfo(String message) {
+        logger.info(message);
+    }
+
+    public static void logWarning(String message) {
+        logger.warning(message);
+    }
+
+    public static void logError(String message, Throwable throwable) {
+        logger.log(Level.SEVERE, message, throwable);
+    }
+
+    public static void logDebug(String message) {
+        logger.fine(message);
+    }
 }
