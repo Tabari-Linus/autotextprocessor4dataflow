f0284c60af4a6df545bf8dae1ad321770aaec011
Mr Lii <leetamparega@gmail.com>
2025-04-29 19:34:10 +0000

    build: setup for project structure. Directories and configureations

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module lii.autotextprocessor4dataflow {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens lii.autotextprocessor4dataflow to javafx.fxml;
+    exports lii.autotextprocessor4dataflow;
+}
