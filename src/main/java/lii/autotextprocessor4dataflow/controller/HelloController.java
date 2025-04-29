0719700ca40f90a5676be47861c0a12709ec33ee
Mr Lii <leetamparega@gmail.com>
2025-04-29 21:40:51 +0000

    docs: update HelloController to handle the introduction page of JavaFX UI

--- src/main/java/lii/autotextprocessor4dataflow/controller/HelloController.java
+++ src/main/java/lii/autotextprocessor4dataflow/controller/HelloController.java
@@ -4,11 +4,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
     @FXML
     private Label welcomeText;
 
+    // Method to handle the button click event
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to AutoTextProcessor for DataFlow!");
     }
 }
