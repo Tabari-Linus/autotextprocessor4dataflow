0a34f95de38d99081398c792374903a2307373db
Mr Lii <leetamparega@gmail.com>
2025-04-29 21:17:06 +0000

    docs: add searchPattern method to call the search method of the regrexservice class to search for possible matches

--- src/main/java/lii/autotextprocessor4dataflow/service/TextProcessor.java
+++ src/main/java/lii/autotextprocessor4dataflow/service/TextProcessor.java
@@ -8,5 +8,8 @@ public class TextProcessor {
         this.regexService = new RegexService();
     }
 
-
+    // Method to search for a pattern in text
+    public boolean searchPattern(String text, String pattern) {
+        return regexService.search(text, pattern);
+    }
 }
