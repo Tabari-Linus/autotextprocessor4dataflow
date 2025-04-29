d2adccbf1d8a64cde3e03d881e5d6120c4c977c3
Mr Lii <leetamparega@gmail.com>
2025-04-29 21:15:23 +0000

    docs: add final RegrexService attribute and add TextProcessor constructor

--- src/main/java/lii/autotextprocessor4dataflow/service/TextProcessor.java
+++ src/main/java/lii/autotextprocessor4dataflow/service/TextProcessor.java
@@ -1,4 +1,12 @@
 package lii.autotextprocessor4dataflow.service;
 
 public class TextProcessor {
+
+    private final RegexService regexService;
+
+    public TextProcessor() {
+        this.regexService = new RegexService();
+    }
+
+
 }
