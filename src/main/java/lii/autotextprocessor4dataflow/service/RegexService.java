82428e88a8b269f080862f110a969164499c71b6
Mr Lii <leetamparega@gmail.com>
2025-04-29 20:40:48 +0000

    docs: add replace  method to RegexService class in service directory which takes in a string text, a regex pattern and a replacement string to replace matches if they are found

--- src/main/java/lii/autotextprocessor4dataflow/service/RegexService.java
+++ src/main/java/lii/autotextprocessor4dataflow/service/RegexService.java
@@ -6,9 +6,17 @@ import java.util.regex.Pattern;
 public class RegexService {
 
     // Method to check if a string matches a given regex pattern
-    public boolean search(String text, String regex) {
+    public boolean search(String inputString, String regex) {
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(text);
+        Matcher matcher = pattern.matcher(inputString);
         return matcher.find();
     }
+
+    public String replace(String inputString, String regexPattern, String replacementString){
+        Pattern pattern = Pattern.compile(regexPattern);
+        Matcher matcher = pattern.matcher(inputString);
+        return matcher.replaceAll(replacementString);
+        }
+
+
 }
